91c3ab6a2e4d75c9b3a94a1ebf0f8c3dee95b5b1
patrick.dalferth <patrick.dalferth@192.168.56.1>
2021-08-04 22:13:24 -0300

    Versao 1.0

--- src/programacao/Arredondamento.java
+++ src/programacao/Arredondamento.java
@@ -10,10 +10,20 @@ package programacao;
  * @author patrick.dalferth
  */
 public class Arredondamento {
-  
     
-    public double arredondarValor(double valor){
-        return 0;
+   public static void main(String[] args){
+        
+        double valor = 9.15;
+        
+        System.out.println("Inteiro mais próximo " + Math.round(valor));
+        System.out.println("Inteiro do número = " + Math.floor(valor));
+        System.out.println("Valor arredondado = " + (double) Math.round(valor * 100) / 100);
+        
+    }
+    
+    //reaproveitamento de código
+    public static double arredondarValor(double valor){
+        return (double) Math.round(valor * 100) / 100;
     }
     
 }
